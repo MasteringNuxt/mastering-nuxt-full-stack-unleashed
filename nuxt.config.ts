1efa651c28e294a84499adaf0d35bd1d7ffcd198
Michael Thiessen <electromikenetic@gmail.com>
2025-03-28 13:46:48 -0400

    Lesson 3-9

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,4 +16,17 @@ export default defineNuxtConfig({
       include: ['debug'],
     },
   },
+
+  mdc: {
+    highlight: {
+      theme: 'material-theme-palenight',
+      langs: [
+        'html',
+        'markdown',
+        'vue',
+        'typescript',
+        'javascript',
+      ],
+    },
+  },
 })
