cef0828be2a40441982bd3a5267dd78f6cd76da7
Michael Thiessen <electromikenetic@gmail.com>
2025-05-14 15:50:58 -0400

    chore: add template files

---
+++ templates/messages/index.post.ts
@@ -0,0 +1,21 @@
+import { createMessageForChat } from '../../../../repository/chatRepository'
+import { CreateMessageSchema } from '../../../../schemas'
+
+export default defineEventHandler(async (event) => {
+  const { id } = getRouterParams(event)
+
+  const { success, data } = await readValidatedBody(
+    event,
+    CreateMessageSchema.safeParse
+  )
+
+  if (!success) {
+    return 400
+  }
+
+  return createMessageForChat({
+    chatId: id,
+    content: data.content,
+    role: data.role,
+  })
+})
