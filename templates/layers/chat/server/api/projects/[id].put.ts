d7698cb06160c80a7ac5a7e70375fc654b5a88d0
Michael Thiessen <electromikenetic@gmail.com>
2025-06-04 10:03:27 -0400

    chore: add template files

---
+++ templates/layers/chat/server/api/projects/[id].put.ts
@@ -0,0 +1,31 @@
+import {
+  updateProject,
+  getProjectById,
+} from '../../repository/projectRepository'
+import { UpdateProjectSchema } from '../../schemas'
+
+export default defineEventHandler(async (event) => {
+  const { id } = getRouterParams(event)
+
+  const { success, data } = await readValidatedBody(
+    event,
+    UpdateProjectSchema.safeParse
+  )
+
+  const project = await getProjectById(id)
+  if (!project) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Project not found',
+    })
+  }
+
+  if (!success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request',
+    })
+  }
+
+  return updateProject(id, data)
+})
